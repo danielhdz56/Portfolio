3c18f981c5b78324ac6f2200859b957e212fc466
Daniel Hernandez <DanielHernandez@Daniels-iMac.local>
2017-07-08 01:37:28 -0500

    Pokemon, hangman, and psychic project info done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+//Email Form
 //Protects against spambots
 $('#contactform').attr('action', 'h' + 't' + 't' + 'p' + 's' + ':' + '/' + '/' + 'f' + 'o' + 'r' + 'm' + 's' + 'p' + 'r' + 'e' + 'e' + '.' + 'i' + 'o' + '/' + 'd' + 'a' + 'n' + 'i' + 'e' + 'l' + '5' + '6' + 'h' + 'd' + 'z' + '@' + 'g' + 'm' + 'a' + 'i' + 'l' + '.' + 'c' + 'o' + 'm');
 $('#next').attr('value', window.location.pathname);
