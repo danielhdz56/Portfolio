13deaf37f7f9b209830dd52f0e90955a782aab90
Daniel Hernandez <DanielHernandez@Daniels-iMac.local>
2017-07-20 16:05:58 -0500

    Added a script that will hold all projects in an object

---
+++ assets/javascript/objects.js
@@ -0,0 +1,14 @@
+var projects = [{
+	name: "hangman",
+	projectImage: "assets/images/world.svg",
+	type: "Bootcamp Homework",
+	date: "Jun 21, 2017 - Jun 27, 2017",
+	description: "Interactive game that dynamically updates the HTML page using JavaScript code.",
+	takeaways: "Manipulated array in order to match, and replace data. Used vanilla Javascript to make dynamic transitions.",
+	exceeded: "Making sure to account for countries that contained spaces, punctuation, prepositions, and conjunctions. Dynamically adding elements on incorrect guesses.",
+	live: "https://danielhdz56.github.io/Hangman-Game/",
+	githubImage: "assets/images/magnifyingGlass.svg",
+	techUsed: "HTML5, CSS3, JS, Bootstrap 3, and Git.",
+	resources: "Stack Overflow, Google, MDN, composition book, and pens.",
+	repo: "https://github.com/danielhdz56/Hangman-Game.git"
+}];
